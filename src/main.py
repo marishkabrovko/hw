401657fe09d1d9688b7f88c9887eac10109b686d
Марина Бровко <marinabrovko@macbook-air-marina.local>
2024-07-11 23:01:09 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_date
 
 if __name__ == "__main__":
     card_number = 7000792289606361
@@ -6,3 +7,12 @@ if __name__ == "__main__":
 
     print(f"Маскированный номер карты: {get_mask_card_number(card_number)}")
     print(f"Маскированный номер счета: {get_mask_account(account_number)}")
+
+    info_card = "Visa Platinum 7000792289606361"
+    info_account = "Счет 73654108430135874305"
+
+    print(f"Маскированный номер карты: {mask_account_card(info_card)}")
+    print(f"Маскированный номер счета: {mask_account_card(info_account)}")
+
+    date_str = "2024-03-11T02:26:18.671407"
+    print(f"Форматированная дата: {get_date(date_str)}")
--- tests/tests_mask.py
+++ tests/tests_mask.py
@@ -1,5 +1,7 @@
 import unittest
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_date
+
 
 class TestMasks(unittest.TestCase):
 
@@ -8,6 +10,17 @@ class TestMasks(unittest.TestCase):
 
     def test_get_mask_account(self):
         self.assertEqual(get_mask_account(73654108430135874305), "**4305")
+
+
+class TestWidget(unittest.TestCase):
+
+    def test_mask_account_card(self):
+        self.assertEqual(mask_account_card("Visa Platinum 7000792289606361"), "Visa Platinum 7000 79** **** 6361")
+        self.assertEqual(mask_account_card("Счет 73654108430135874305"), "Счет **4305")
+
+    def test_get_date(self):
+        self.assertEqual(get_date("2024-03-11T02:26:18.671407"), "11.03.2024")
+
 
 if __name__ == '__main__':
     unittest.main()
