aedd9ef189daf144e120b42a3d3bbbca928ef2be
marinabrovko <marishka.brovko@gmail.com>
2024-07-18 14:56:10 +0300

    исправления тестов

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -16,6 +16,5 @@ def test_get_mask_card_number(card_number, expected):
     (12345678901234567890, "**7890"),
     (1234, "1234")
 ])
-
 def test_get_mask_account(account_number, expected):
     assert get_mask_account(account_number) == expected
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,18 +1,70 @@
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+import pytest
+from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(transactions):
-    assert len(filter_by_state(transactions)) == 2
-    assert len(filter_by_state(transactions, "CANCELED")) == 2
-    assert len(filter_by_state(transactions, "PENDING")) == 0
+@pytest.mark.parametrize("data, state, expected", [
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        'EXECUTED',
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        ]
+    ),
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        'CANCELED',
+        [
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ]
+    )
+])
+def test_filter_by_state(data, state, expected):
+    assert filter_by_state(data, state) == expected
 
 
-def test_sort_by_date(transactions):
-    sorted_transactions = sort_by_date(transactions)
-    assert sorted_transactions[0]["id"] == 41428829
-    assert sorted_transactions[-1]["id"] == 939719570
-
-    sorted_transactions_asc = sort_by_date(transactions, descending=False)
-    assert sorted_transactions_asc[0]["id"] == 939719570
-    assert sorted_transactions_asc[-1]["id"] == 41428829
+@pytest.mark.parametrize("data, descending, expected", [
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        True,
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        ]
+    ),
+    (
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        False,
+        [
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+        ]
+    )
+])
+def test_sort_by_date(data, descending, expected):
+    assert sort_by_date(data, descending) == expected
