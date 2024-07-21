a093a73159e4854e95c18eedb9c482d14b0bd6bb
marinabrovko <marishka.brovko@gmail.com>
2024-07-21 23:01:15 +0300

    новое дз

---
+++ src/generators.py
@@ -0,0 +1,20 @@
+from typing import List, Dict, Iterator
+
+
+def filter_by_currency(transactions: List[Dict], currency: str) -> Iterator[Dict]:
+    """ Фильтрует транзакции по указанной валюте."""
+    for transaction in transactions:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+            yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict]) -> Iterator[str]:
+    """ Генерирует описания транзакций. """
+    for transaction in transactions:
+        yield transaction.get("description", "")
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """ Генерирует номера банковских карт в указанном диапазоне. """
+    for number in range(start, stop + 1):
+        yield f"{number:016d}"[:4] + " " + f"{number:016d}"[4:8] + " " + f"{number:016d}"[8:12] + " " + f"{number:016d}"[12:]
