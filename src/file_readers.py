39c9af21103f407ea2898aae9d1d3bf0fdb82aa1
marinabrovko <marishka.brovko@gmail.com>
2024-08-10 22:18:47 +0300

    add readers+files

---
+++ src/file_readers.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from typing import List, Dict
+
+CSV_FILE_PATH = "data/transactions.csv"
+EXCEL_FILE_PATH = "data/transactions_excel.xlsx"
+
+
+def read_transactions_from_csv(file_path: str = CSV_FILE_PATH) -> List[Dict[str, str]]:
+    """
+    Считывает финансовые операции из CSV-файла.
+
+    Args:
+        file_path (str): Путь к CSV-файлу.
+
+    Returns:
+        List[Dict[str, str]]: Список словарей с транзакциями.
+    """
+    try:
+        df = pd.read_csv(file_path)
+        return df.to_dict(orient='records')
+    except (FileNotFoundError, pd.errors.EmptyDataError):
+        return []
+
+
+def read_transactions_from_excel(file_path: str = EXCEL_FILE_PATH) -> List[Dict[str, str]]:
+    """
+    Считывает финансовые операции из Excel-файла.
+
+    Args:
+        file_path (str): Путь к Excel-файлу.
+
+    Returns:
+        List[Dict[str, str]]: Список словарей с транзакциями.
+    """
+    try:
+        df = pd.read_excel(file_path)
+        return df.to_dict(orient='records')
+    except (FileNotFoundError, ValueError):
+        return []
